d7838b98e8cc38254b5e334e9166a3880cbf52e5
Rafael <rafael.inacio0196@gmail.com>
2020-05-24 20:08:58 -0300

    PicPay FrontEnd

---
+++ src/screens/Home/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
+
+import {
+  Wrapper,
+  Header,
+  Container,
+  BalanceContainer,
+  BalanceTitle,
+  Balance,
+} from "./styles";
+
+import Suggestions from '../../components/Suggestions';
+import Activities from '../../components/Activities';
+import Tips from '../../components/Tips';
+import Banner from '../../components/Banner';
+
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons
+            name="qrcode-scan"
+            size={30}
+            color="#10c863"
+          />
+          <BalanceContainer>
+            <BalanceTitle>Meu saldo</BalanceTitle>
+            <Balance>R$0,00</Balance>
+          </BalanceContainer>
+          <AntDesign name="gift" size={30} color="#10c863" />
+        </Header>
+        <Suggestions />
+        <Activities />
+        <Tips />
+        <Banner />
+      </Container>
+    </Wrapper>
+  );
+}
